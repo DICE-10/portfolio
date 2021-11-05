27a779ebbbd82970041d110fef24cb0952c3e8c2
dice <ds_work_prog@outlook.jp>
2021-11-05 22:30:08 +0900

    type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/freestyle.min.css'
-import '../styles/freestyle-typewriter.min.css'
+//import '../styles/freestyle-typewriter.min.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
